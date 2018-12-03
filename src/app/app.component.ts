58455d545468e8a3b6f4175510663738c11d02eb
Alejandro Riveros <lar4892@gmail.com>
2018-12-03 08:24:53 -0400

    productos angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'productos-angular-lab';
+  datos = [
+    {codigo: 1, descripcion: "papas", precio: 10.55 },
+    {codigo: 2, descripcion: "manzanas", precio: 12.1 },
+    {codigo: 3, descripcion: "melon", precio: 52.3 },
+    {codigo: 4, descripcion: "cebollas", precio: 17 },
+    {codigo: 5, descripcion: "calabazas", precio: 20 }
+  ];
 }
